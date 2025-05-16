f21f2c57a0063988901b63fc3c728d6a7891d407
Mourad <mourad.arejdal@gmail.com>
2025-05-16 16:46:32 +0100

    Remove .env file from version control

--- UserService/src/main/java/com/example/userservice/repository/ModelHasRolesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservice.repository;
-
-import com.example.userservice.model.ModelHasRoles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ModelHasRolesRepository extends JpaRepository<ModelHasRoles, Long> {
-
-}
